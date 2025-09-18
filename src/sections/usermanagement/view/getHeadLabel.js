9149c3f2285a7f6c632f404d4277cdaf55140af7
Saurabh Pandey <saurabh.pandey@spacetotech.com>
2025-09-18 12:09:51 +0530

    updated

--- src/sections/usermanagement/view/getHeadLabel.js
+++ src/sections/usermanagement/view/getHeadLabel.js
@@ -7,14 +7,14 @@ export const headLabel = (selectedTab) => {
           id: "name",
           name: "username",
           label: "Name",
-          minWidth: 150,
+          minWidth: 300,
           sortable: true,
         },
         {
           id: "email",
           name: "email",
           label: "Email",
-          minWidth: 150,
+          minWidth: 300,
           sortable: true,
         },
         { id: "action", label: "Action", minWidth: 180 },
@@ -26,43 +26,43 @@ export const headLabel = (selectedTab) => {
           id: "name",
           name: "username",
           label: "Name",
-          minWidth: 150,
+          minWidth: 300,
           sortable: true,
         },
         {
           id: "email",
           name: "email",
           label: "Email",
-          minWidth: 150,
+          minWidth: 300,
           sortable: true,
         },
         { id: "action", label: "Action", minWidth: 180 },
       ];
     case 2:
       return [
-        { id: "sno", label: "S.No.", minWidth: 90, align: "center" },
+        { id: "sno", label: "S.No.", minWidth: 60, align: "center" },
         {
           id: "name",
           name: "username",
           label: "Name",
-          minWidth: 150,
+          minWidth: 180,
           sortable: true,
         },
         {
           id: "email",
           name: "email",
           label: "Email",
-          minWidth: 150,
+          minWidth: 180,
           sortable: true,
         },
         {
           id: "mastersheetPermissions",
           name: "mastersheetPermissions",
           label: "MasterSheet Permissions",
-          minWidth: 200,
+          minWidth: 300,
           sortable: true,
         },
-        { id: "action", label: "Action", minWidth: 180 },
+        { id: "action", label: "Action", minWidth: 150 },
       ];
     case 3:
       return [
