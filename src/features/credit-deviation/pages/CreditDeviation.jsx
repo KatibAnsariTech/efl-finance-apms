0f27d5137e8df955f68697e70e0c3f5e4bce5230
Saurabh Pandey <saurabh.pandey@spacetotech.com>
2025-09-19 18:56:24 +0530

    updated

--- src/features/credit-deviation/pages/CreditDeviation.jsx
+++ src/features/credit-deviation/pages/CreditDeviation.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import CreditDeviationDashboard from './credit-deviation-dashboard';
+import CreditDeviationDashboard from '../components/CreditDeviationDashboard';
 
 export default function CreditDeviationPage() {
   return (
