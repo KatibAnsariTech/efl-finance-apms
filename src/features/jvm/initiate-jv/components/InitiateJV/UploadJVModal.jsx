b43e64f2a84f90e2a370101e584025e928f51726
Saurabh Pandey <saurabh.pandey@spacetotech.com>
2025-10-01 19:19:15 +0530

    updated

--- src/features/jvm/initiate-jv/components/InitiateJV/UploadJVModal.jsx
+++ src/features/jvm/initiate-jv/components/InitiateJV/UploadJVModal.jsx
@@ -185,6 +185,7 @@ export default function UploadJVModal({ open, onClose, onSuccess }) {
               'personalNumber': 'personalNumber',
               'remarks': 'remarks',
               'postingDate': 'postingDate'
+              
             };
             
             // Case-insensitive field mapping
